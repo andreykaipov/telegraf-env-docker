d7d7069660f6ae5daadb66d9bbe6b9694c277ca3
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2019-08-23 10:13:28 -0400

    use env2conf to properly parse out toml from the environment

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 ARG base=latest
 FROM telegraf:$base
-COPY *.sh /opt/
-ENTRYPOINT ["/opt/entrypoint.sh"]
+RUN wget -O env2conf https://github.com/andreykaipov/env2conf/releases/download/v0.2.0/env2conf-0.2.0-linux-amd64 && \
+    chmod +x env2conf && \
+    mv env2conf /usr/local/bin
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
 CMD ["telegraf"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,15 @@
 
 : "${config_path:=/etc/telegraf/telegraf.conf}"
 mkdir -p "$(dirname $config_path)"
-"$(dirname "$0")/telegraf.conf.sh" > $config_path
+
+env2conf -prefix inputs,processors,aggregators,outputs -output toml > /opt/telegraf.conf
+
+if [ -s /opt/telegraf.conf ]; then
+    nonempty='yes'
+fi
 
 if echo "$@" | grep -q ^-; then
-    set -- telegraf "$@"
+    set -- telegraf ${nonempty:+--config /opt/telegraf.conf} "$@"
 fi
 
 exec "$@"
